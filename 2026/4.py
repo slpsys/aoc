fdad19356374ab917ae394e6988ce14549e873de
Marc Bollinger <mbollinger@thumbtack.com>
2025-12-21 19:32:43 -0500

    Converting to point class

--- 2026/4.py
+++ 2026/4.py
@@ -1,29 +1,35 @@
 import sys
 
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        print(f"({self.x}, {self.y})")
+
 class BoundingBox:
-    def __init__(self, nw_x=0, nw_y=0, se_x=0, se_y=0):
-        self.nw_x = nw_x
-        self.nw_y = nw_y
-        self.se_x = se_x
-        self.se_y = se_y
+    def __init__(self, nw: Point=None, se: Point=None):
+        self.nw = nw if nw else Point(0, 0)
+        self.se = se if se else Point(0, 0)
     
     def __str__(self) -> str:
-        return f"({self.nw_x}, {self.nw_y}) - ({self.se_x}, {self.se_y})"
+        return f"({self.nw.x}, {self.nw.y}) - ({self.se.x}, {self.se.y})"
 
-def safe_offset(x, y, x_max, y_max, x_off=1, y_off=1) -> BoundingBox:
+def safe_offset(p: Point, x_max, y_max, x_off=1, y_off=1) -> BoundingBox:
     b = BoundingBox()
-    b.nw_x = (x - x_off) if (x - x_off > 0) else 0
-    b.nw_y = (y - y_off) if (y - y_off > 0) else 0 
-    b.se_x = (x + x_off) if (x + x_off < x_max) else x_max
-    b.se_y = (y + y_off) if (y + y_off < y_max) else y_max
+    b.nw.x = (p.x - x_off) if (p.x - x_off > 0) else 0
+    b.nw.y = (p.y - y_off) if (p.y - y_off > 0) else 0 
+    b.se.x = (p.x + x_off) if (p.x + x_off < x_max) else x_max
+    b.se.y = (p.y + y_off) if (p.y + y_off < y_max) else y_max
     return b
 
-def assess_cell(rows: list[str], x: int, y: int, box: BoundingBox, on_char: str) -> int:
+def assess_cell(rows: list[str], p: Point, box: BoundingBox, on_char: str) -> int:
     val = 0
 
-    for ix in range(box.nw_x, box.se_x + 1):
-        for iy in range(box.nw_y, box.se_y + 1):
-            if x == ix and y == iy:
+    for ix in range(box.nw.x, box.se.x + 1):
+        for iy in range(box.nw.y, box.se.y + 1):
+            if p.x == ix and p.y == iy:
                 continue
 
             if rows[iy][ix] == on_char:
@@ -36,20 +42,17 @@ def assess_rolls(rows: list[str], on_char: str = "@") -> int:
     # Assme rectangular
     height = len(rows)
     width = len(rows[0])
-    accessible = 0
-
-    debug_rows = []
+    accessible = []
 
     for row in range(height):
-        debug_rows.append(rows[row])
         for column in range(width):
             if rows[row][column] != on_char:
                 continue
 
-            bounding_box = safe_offset(column, row, width - 1, height - 1)
-            score = assess_cell(rows, column, row, bounding_box, on_char)
+            bounding_box = safe_offset(Point(column, row), width - 1, height - 1)
+            score = assess_cell(rows, Point(column, row), bounding_box, on_char)
             if score < 4:
-                accessible += 1
+                accessible.append(Point(column, row))
     return accessible
 
 grid_rows = []
@@ -58,4 +61,4 @@ for line in sys.stdin:
     grid_rows.append(ls)
 
 accessible_rolls = assess_rolls(grid_rows)
-print(accessible_rolls)
+print(len(accessible_rolls))
